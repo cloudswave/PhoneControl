d7dce6a1fb276ff83a635e796bef5753719ddd2e
EthanZhu <764724624@qq.com>
2026-04-04 15:34:01 +0000

    feat: 更新应用名称和标识符为“安卓群控”

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,12 @@
-import { ServerList } from './ServerList';
-import { FpsSlider } from './FpsSlider';
-import { DeviceList } from './DeviceList';
-import styles from './Sidebar.module.css';
+import { ServerList } from "./ServerList";
+import { FpsSlider } from "./FpsSlider";
+import { DeviceList } from "./DeviceList";
+import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <div className={styles.logo}>Phone Control</div>
+      <div className={styles.logo}>安卓群控</div>
       <div className={styles.divider} />
       <ServerList />
       <div className={styles.divider} />
